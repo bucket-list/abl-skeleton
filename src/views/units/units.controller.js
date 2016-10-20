2a5ecbc325804812933fcabdd5c572494744df2b
Adam Ralko <adam@ralko.com>
2016-10-20 16:47:00 -0700

    Added units example for adding sample data to local test API server and accessing client-side via ng-feathers.

--- src/views/units/units.controller.js
+++ src/views/units/units.controller.js
@@ -3,6 +3,18 @@
  */
 
 function UnitsController ($scope, users, config, $feathers) {
+  var vm = this;
 
+  $scope.units = [];
+  $scope.unitService = $feathers.service('units');
+
+  vm.unitFeathers = function() {
+    console.unit($feathers);
+  };
+
+  // Get units
+  $scope.unitService.find({}).then(function(units) {
+    console.log(units);
+  });
 
 }
--- test/server.js
+++ test/server.js
@@ -6,12 +6,6 @@ const socketio = require('feathers-socketio');
 const bodyParser = require('body-parser');
 const service = require('feathers-nedb');
 
-// Logging db
-const logDb = new NeDB({
-  filename: './test/api/log',
-  autoload: true
-});
-
 // Create a feathers instance.
 var app = feathers()
   // Enable REST services
@@ -24,6 +18,38 @@ var app = feathers()
   .use(bodyParser.urlencoded({extended: true}));
 
 
+const genUnit =
+{
+   strings: {
+   en: {
+         title: "Deluxe Ocean View",
+         description: "This unit can see the ocean and has a beautiful decor."
+     } // other languages like french, spanish, etc
+   },
+   size: { amount: 450, unit: "sq ft" },
+   guests: 4, // suitable number of guests
+   amenities: [
+     { _id: "AMEN_ID1", name: "satellite tv" },
+     { _id: "AMEN_ID2", name: "broadband internet" },
+     { _id: "AMEN_ID3", name: "parking" }
+   ],
+   images: [ "unit-image-url-1.jpg", "unit-image-url-2.jpg" ], // all unit images
+   rooms: { bedroom: 2, bathroom: 2, kitchen: 1, laundry: 1, parking: 1 },
+   beds: { king: 1, queen: 1, double: 1, sofa: 1 },
+   units: [
+     { _id: "UNIT_ID_1", name: "1" },
+     { _id: "UNIT_ID_2", name: "2" },
+     { _id: "UNIT_ID_3", name: "3" }
+   ],
+   property: "PROPERTY_ID"
+ };
+
+// Logging db
+const logDb = new NeDB({
+  filename: './test/api/log',
+  autoload: true
+});
+
 // Connect to the db, create and register a Feathers service for logging db.
 app.use('/log', service({
   Model: logDb,
@@ -33,17 +59,39 @@ app.use('/log', service({
   }
 }));
 
-// Log server start
+// Units db
+const unitDb = new NeDB({
+  filename: './test/api/units',
+  autoload: true
+});
+
+app.use('/units', service({
+  Model: unitDb,
+  paginate: {
+    default: 10,
+    max: 1000
+  }
+}));
+
+// Generate local db from mock .js data
+// var units = require('./api/unit.samples.js');
+// units.forEach(function(unit) {
+//   app.service('units').create(unit);
+// });
+
+// Start the server.
+const port = 3030;
+app.listen(port, function() {
+  console.log(`API listening localhost:${port}`);
+});
+
 app.service('log').create({
-  text: 'START',
+  text: "START",
   createdAt: Date.now()
 }).then(function(log) {
   console.log('log:', JSON.stringify(log));
 });
 
-// Start the server.
-const port = 3030;
 
-app.listen(port, function() {
-  console.log(`API listening localhost:${port}`);
-});
+
+module.exports = app;
