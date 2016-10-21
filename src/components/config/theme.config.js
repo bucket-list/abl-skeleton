f2f59a59e19871b49dbe1efafc68686edda5ff01
Adam Ralko <adam@ralko.com>
2016-10-21 16:05:42 -0700

    Refactored theme configuration to own file.

---
+++ src/components/config/theme.config.js
@@ -0,0 +1,24 @@
+function theme ($mdThemingProvider) {
+  $mdThemingProvider.alwaysWatchTheme(true);
+
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('purple')
+    .warnPalette('red')
+    .backgroundPalette('grey');
+
+  $mdThemingProvider
+   .theme('newTheme')
+   .primaryPalette('red')
+   .accentPalette('teal')
+   .warnPalette('pink')
+   .backgroundPalette('grey');
+
+  // Enable browser color for mobile devices.
+  $mdThemingProvider.enableBrowserColor({
+    theme:   'default',
+    palette: 'accent',
+    hue:     '800'
+  });
+}
