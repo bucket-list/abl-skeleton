8ffd5d2308ab67aefda09c589ada368be1d0194d
Adam Ralko <adam@ralko.com>
2016-10-20 21:58:45 -0700

    Updated units example view.

--- src/views/units/units.controller.js
+++ src/views/units/units.controller.js
@@ -5,17 +5,17 @@
 function UnitsController ($scope, users, config, $feathers, $log) {
   var vm = this;
   vm.units = [];
+
   $scope.unitService = $feathers.service('units');
 
   // Get units
   $scope.unitService.find({}).then(function(units) {
     vm.units = units.data;
-    $log.debug(vm.units);
+    $log.debug('Units', units);
     $scope.safeApply();
   });
 
   vm.logUnits = function() {
     $log.debug(vm.units);
   };
-
 }
